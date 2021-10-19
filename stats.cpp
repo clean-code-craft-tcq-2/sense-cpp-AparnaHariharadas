b73582eb3509f2262fa798f04d70e283966109c0
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2021-10-19 13:18:21 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,4 +2,14 @@
 
 Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
     //Implement statistics here
+    const std::vector<__>& inputlist;
+    average = ComputeStatistics.average(inputlist);
+    max = ComputeStatistics.max(inputlist);
+    min = ComputeStatistics.min(inputlist);
+}
+
+float ComputeStatistics.average(std::vector<___>& elementlist)
+{
+    sum = std::accumulate(elementlist.begin(),elementlist.end(),0.0);
+    avg = sum/elementlist.size();
 }
