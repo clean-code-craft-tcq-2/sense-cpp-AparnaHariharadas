e3d793edf22a18e5ee325d3d1c32ae7c75438a90
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2021-10-19 13:18:14 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -3,3 +3,14 @@
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<___>& );
 }
+
+class ComputeStatistics : public const std::vector<___>&
+{
+    public:
+    float  average(std::vector<__>& element_list);
+    float  max(std::vector<__>& element_list);
+    float  min(std::vector<__>& element_list);
+}
+
+
+    
