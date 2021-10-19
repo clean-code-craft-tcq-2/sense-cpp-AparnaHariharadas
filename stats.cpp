3f6855cbaa6202c6bdbaf14ab08295c722fd7322
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2021-10-19 16:12:11 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -6,7 +6,7 @@ float Statistics::ComputeStatistics(const std::vector<float>& elementlist) {
     //const std::vector<__>& elementlist;
     //isnan(elemnetlist)
     float sum = std::accumulate(elementlist.begin(),elementlist.end(),0.0);
-    float average = sum/elementlist.size();
+    average = sum/elementlist.size();
     //max = ComputeStatistics.max(inputlist);
    // min = ComputeStatistics.min(inputlist);
     return average;
