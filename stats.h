d422f16f82e25c833756cffe7ed997d5b853f143
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2021-10-20 15:35:47 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,8 +1,7 @@
 #include <vector>
-//#define Stats float
 namespace Statistics {
    float ComputeStatistics(const std::vector<float>& elementlist);
-   float average1();
+   float average1;
    class ComputeStatistics
 {
 public:
