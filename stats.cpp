2fc90376657b0ead43b5803efd52def3b5a9a663
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2021-10-22 21:41:57 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,6 +3,50 @@
 #include <math.h>
 #include <algorithm>
 using namespace std;
+
+class EmailAlert {
+  public:
+   bool emailSent;
+   
+};
+
+class LEDAlert {
+   public:
+   bool ledGlows;
+   
+};
+class StatsAlerter {
+   public:
+   float maxThreshold;
+   std::vector<bool> alerters;
+   public:
+   StatsAlerter(float maxThreshold1,std::vector<bool> alerters1)
+   {
+      maxThreshold = maxThreshold1;
+      alerters = alerters1;
+   }
+   
+   EmailAlert email;
+   LEDAlert led;
+   StatsAlerter checkAndAlert(const std::vector<float>& elementlist)
+   {
+       auto max = (float)*max_element(begin(elementlist),end(elementlist));
+       std::vector<bool> alerters1 =alerters;
+       float maxThreshold1 = maxThreshold;
+       EmailAlert email;
+       LEDAlert led;
+       if (max > maxThreshold1) {
+        
+           email.emailSent = true;
+           led.ledGlows = true;
+       }
+       else{
+           email.emailSent = false;
+           led.ledGlows = false;
+    }
+}
+   
+};
 Statistics::Stats Statistics::ComputeStatistics(const vector<float>& elementlist) {
     //Implement statistics here
     //isnan(elemnetlist)
