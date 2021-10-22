5fdc0e300a5fc684b2be18b4b7b1ee94bac2653f
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2021-10-22 19:48:18 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -10,7 +10,23 @@ public:
    Statistics::Stats ComputeStatistics(const std::vector<float>& elementlist);
 
 }
+class EmailAlert {
+  bool emailSent;
+   
+};
 
+class LEDAlert {
+   bool ledGlows;
+   
+};
+class StatsAlerter {
+   public:
+   float maxThreshold;
+   EmailAlert email;
+   LEDAlert led;
+   void checkAndAlert(const std::vector<<IAlerter*>& alerters);
+   
+};
 
 
     
