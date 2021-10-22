8e6455f56ab88be373197fc95157732c3c7a797d
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2021-10-22 18:11:40 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,7 +2,6 @@
 #include <numeric>
 #include <math.h>
 #include <algorithm>
-//extern float average1;
 using namespace std;
 Statistics::Stats Statistics::ComputeStatistics(const vector<float>& elementlist) {
     //Implement statistics here
@@ -10,9 +9,7 @@ Statistics::Stats Statistics::ComputeStatistics(const vector<float>& elementlist
     Statistics::Stats computedStats;
     int length_list = elementlist.size();
     float sum = accumulate(elementlist.begin(),elementlist.end(),0.0);
-   // auto max = std::max_element(std::begin(elementlist),std::end(elementlist));
-   // auto min = std::min_element(elementlist.begin(),elementlist.end());
-    computedStats.average1 = (float)sum/length_list;
+    computedStats.average = (float)sum/length_list;
     computedStats.max = (float)*max_element(begin(elementlist),end(elementlist));
     computedStats.min = (float)*min_element(begin(elementlist),end(elementlist));
     return computedStats;
