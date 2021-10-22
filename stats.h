3c932a6ea3713ef5aca6f38be3e06b2907b11b75
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2021-10-22 21:42:11 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -11,33 +11,8 @@ public:
 
 }
 
-class EmailAlert {
-  public:
-   bool emailSent;
-   
-};
 
-class LEDAlert {
-   public:
-   bool ledGlows;
-   
-};
-class StatsAlerter {
-   public:
-   float maxThreshold;
-   std::vector<bool> alerters;
-   public:
-   StatsAlerter(float maxThreshold1,std::vector<bool> alerters1)
-   {
-      maxThreshold = maxThreshold1;
-      alerters = alerters1;
-   }
-   
-   EmailAlert email;
-   LEDAlert led;
-   StatsAlerter checkAndAlert(const std::vector<float>& elementlist);
-   
-}SAlert;
+
 
 
     
