7125fd8b8f7cb4f45892f7a6a77880e728fcb71b
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2021-10-22 18:53:27 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -9,9 +9,20 @@ Statistics::Stats Statistics::ComputeStatistics(const vector<float>& elementlist
     Statistics::Stats computedStats;
     int length_list = elementlist.size();
     float sum = accumulate(elementlist.begin(),elementlist.end(),0.0);
+    bool NAN_check = elementlist.empty();
+    const float NAN = sqrt(-1);
+    if(!NAN_check)
+    {
     computedStats.average = (float)sum/length_list;
     computedStats.max = (float)*max_element(begin(elementlist),end(elementlist));
     computedStats.min = (float)*min_element(begin(elementlist),end(elementlist));
+    }
+    else
+    {
+        computedStats.average = NAN;
+        computedStats.average = NAN;
+        computedStats.average = NAN;
+    }
     return computedStats;
 }
 
