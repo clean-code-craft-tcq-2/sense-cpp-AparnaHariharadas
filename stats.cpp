b1306f809bb59626b82f74d7efa4d68d78ce5af0
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2021-10-22 18:00:48 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,17 +3,18 @@
 #include <math.h>
 #include <algorithm>
 //extern float average1;
-Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& elementlist) {
+using namespace std;
+Statistics::Stats Statistics::ComputeStatistics(const vector<float>& elementlist) {
     //Implement statistics here
     //isnan(elemnetlist)
     Statistics::Stats computedStats;
     int length_list = elementlist.size();
-    float sum = std::accumulate(elementlist.begin(),elementlist.end(),0.0);
+    float sum = accumulate(elementlist.begin(),elementlist.end(),0.0);
    // auto max = std::max_element(std::begin(elementlist),std::end(elementlist));
    // auto min = std::min_element(elementlist.begin(),elementlist.end());
     computedStats.average1 = (float)sum/length_list;
-    computedStats.max = (float)std::*max_element(std::begin(elementlist),std::end(elementlist));
-    computedStats.min = (float)std::min_element(elementlist.begin(),elementlist.end());
+    computedStats.max = (float)*max_element(begin(elementlist),end(elementlist));
+    computedStats.min = (float)min_element(elementlist.begin(),elementlist.end());
     return computedStats;
 }
 
