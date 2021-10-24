b0b1658e0af2497097304e9b0c9c5b92fe8a2500
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2021-10-24 22:38:36 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,15 +1,13 @@
 #include "stats.h"
 
-
-//extern float StatsAlerter::maxThreshold;
 Statistics::Stats Statistics::ComputeStatistics(const vector<float>& elementlist) {
     //Implement statistics here
-    //isnan(elemnetlist)
+   
     Statistics::Stats computedStats;
     int length_list = elementlist.size();
     float sum = accumulate(elementlist.begin(),elementlist.end(),0.0);
     bool NAN_check = elementlist.empty();
-    //const double NAN = sqrt(-1);
+    
     if(!NAN_check)
     {
     computedStats.average = (float)sum/length_list;
@@ -24,29 +22,3 @@ Statistics::Stats Statistics::ComputeStatistics(const vector<float>& elementlist
     }
     return computedStats;
 }
-
-/*int checkAndAlert(const vector<float>& elementlist)
-{
-    auto max = (float)*max_element(begin(elementlist),end(elementlist));
-    cout << StatsAlerter::maxThreshold <<endl;
-    //std::vector<bool> alerters1 =alerters;
-    float maxThreshold1 =10.4;// maxThreshold;
-        int val;
-    EmailAlert email;
-    LEDAlert led;
-    if (max > maxThreshold1) {
-        
-        email.emailSent = true;
-        led.ledGlows = true;
-    }
-    else{
-        email.emailSent = false;
-        led.ledGlows = false;
-    }
-        if (max > maxThreshold1) {
-            val =0;
-        }
-    return val;
-}*/
-        
-  
